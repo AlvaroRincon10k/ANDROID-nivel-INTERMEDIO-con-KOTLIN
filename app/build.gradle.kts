184f39ad32d600e1a2982993ec65a8e97a125e53
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-06-19 19:53:38 -0500

    Contenedor de fragments.
    - Se agrega el "FragmentContainerView" a la vista principal que es "activiy_main".
    - Se cambia el color de fondo al layout "fragmente_horoscope".
    - Se cambia el compileSdk y el targetSdk a 34 para que sea compatible con la librería navigation-fragment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.horoscapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.horoscapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
