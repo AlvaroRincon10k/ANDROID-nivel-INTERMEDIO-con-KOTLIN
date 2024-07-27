796e41949ee8be35d20671ce08e6601de308a46d
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-07-26 19:54:23 -0500

    Configuracion de dagger hilt.
    - Se añaden las dependencias necesarias para dagger hilt.
    - Se crea la clase "HoroscApp".
    - Se añade la clase "HoroscApp" al archivo "AndroidManifest".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -37,6 +39,9 @@ android {
     buildFeatures{
         viewBinding = true
     }
+    kotlin {
+        jvmToolchain(8)
+    }
 }
 
 dependencies {
@@ -54,4 +59,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
 }
