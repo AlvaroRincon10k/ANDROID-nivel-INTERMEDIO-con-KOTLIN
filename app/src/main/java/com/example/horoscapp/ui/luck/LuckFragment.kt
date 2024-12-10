4b067eac5f708f8ad5be8fc5ec2a8fa79247ddb5
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-12-09 20:22:13 -0500

    Animación slideCard
    - Se agrega la imagen "card_back_up" para la aminación.
    - Se crea el archivo xml "slide_up" de tipo animación.
    - Se agrega un imageView a la vista "fragment_luck".
    - Se crea la función "slideCard" para agregar la animación.

--- app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horoscapp/ui/luck/LuckFragment.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.example.horoscapp.R
 import com.example.horoscapp.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.Random
@@ -38,7 +43,24 @@ class LuckFragment : Fragment() {
             ObjectAnimator.ofFloat(binding.ivRoulette, View.ROTATION, 0f, degrees.toFloat())
         animator.duration = 2000
         animator.interpolator = DecelerateInterpolator()
+        animator.doOnEnd { slideCard() }
         animator.start()
+    }
+
+    private fun slideCard() {
+        val slideUpAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.slide_up)
+        slideUpAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+                binding.reverse.isVisible = true
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {}
+
+            override fun onAnimationRepeat(p0: Animation?) {}
+
+        })
+
+        binding.reverse.startAnimation(slideUpAnimation)
     }
 
     override fun onCreateView(
