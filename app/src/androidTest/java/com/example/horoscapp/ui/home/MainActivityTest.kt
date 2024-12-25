c942314d377a8ae46300b2cd477a4e171caa32de
Alvaro Rincon <alvarorinconc1@gmail.com>
2024-12-25 14:40:40 -0500

    Test UI Intent.
    - Se crea un test para los intents
    - Se inicializan los intents y se liberan al final.

--- app/src/androidTest/java/com/example/horoscapp/ui/home/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/horoscapp/ui/home/MainActivityTest.kt
@@ -1,13 +1,20 @@
 package com.example.horoscapp.ui.home
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.horoscapp.R
+import com.example.horoscapp.ui.detail.HoroscopeDetailActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -16,7 +23,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
-class MainActivityTest{
+class MainActivityTest {
 
     @get:Rule(order = 0)
     val hiltRule = HiltAndroidRule(this)
@@ -25,12 +32,29 @@ class MainActivityTest{
     var mainActivityRule = ActivityScenarioRule(MainActivity::class.java)
 
     @Before
-    fun setUp(){
+    fun setUp() {
         hiltRule.inject()
+        Intents.init()
+    }
+
+    @After
+    fun tearDown(){
+        Intents.release()
     }
 
     @Test
-    fun when_mainactivity_is_created_then_open_luck_fragment(){
+    fun when_mainactivity_is_created_then_open_luck_fragment() {
         onView(withId(R.id.luckFragment)).perform(click())
+    }
+
+    @Test
+    fun when_horoscope_is_selected_then_opne_detail() {
+        onView(withId(R.id.rvHoroscope)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        intended(hasComponent(HoroscopeDetailActivity::class.java.name))
     }
 }
